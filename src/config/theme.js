80a473f5b17e0ce99d87275dbcb82b436513a274
Trey Hoover <treyhoover@gmail.com>
2016-06-08 00:53:01 -0400

    Use red theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,7 +1,7 @@
 import {
-  cyan500, cyan700,
+  red500, red700,
   grey100, grey300, grey400, grey500,
-  pinkA200,
+  redA200,
   white, darkBlack, fullBlack
 } from 'material-ui/styles/colors';
 import { fade } from 'material-ui/utils/colorManipulator';
@@ -22,10 +22,10 @@ const theme = {
   },
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    primary1Color: cyan500,
-    primary2Color: cyan700,
+    primary1Color: red500,
+    primary2Color: red700,
     primary3Color: grey400,
-    accent1Color: pinkA200,
+    accent1Color: redA200,
     accent2Color: grey100,
     accent3Color: grey500,
     textColor: darkBlack,
@@ -33,7 +33,7 @@ const theme = {
     canvasColor: white,
     borderColor: grey300,
     disabledColor: fade(darkBlack, 0.3),
-    pickerHeaderColor: cyan500,
+    pickerHeaderColor: red500,
     clockCircleColor: fade(darkBlack, 0.07),
     shadowColor: fullBlack
   }
